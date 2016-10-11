1949a438ad06e6fd29bd0550c2dd6336e289b118
R Coyotl <coyotl101093@gmail.com>
2016-10-10 22:55:31 -0400

    Update ProjectE3.cpp

--- ProjectE3.cpp
+++ ProjectE3.cpp
@@ -1,3 +1,5 @@
+//What is the largest prime factor of the number 600851475143 ?
+
 #include <iostream>
 
 using namespace std;
@@ -6,8 +8,15 @@ int main() {
   long n = 600851475143;
   for (long i=2; i*i < n; i++) {
     while (n % i == 0) {
-      if (n>i) n /= i;
+      n /= i;
     }
   }
   cout << "Largest prime factor: " << n << endl;
 }
+
+
+/*If any given number is divided by the smallest possible factor, it leaves behind the largest possible factor.
+  example: 100/ 2 = 50; 50 is the largest possible factor and 2 is the smallest. 
+  Continuously dividing the largest remaining factor, will eventually lead you to the largest prime factor. 
+
+ 
